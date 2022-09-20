fbc4760cf3da32a9cbe6236fa37a4f9930401163
Adam Dilger <adam.dilger@geometry.com.au>
2022-09-20 16:57:48 +1000

    queue

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - 1 - i; j++) {
+			const nextIndex = j + 1;
+			if (arr[nextIndex] < arr[j]) {
+				const tmp = arr[nextIndex];
+				arr[nextIndex] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
 }
