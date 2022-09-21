bb677cb9d657eae2c81a3515165dd50e897f46be
Adam Dilger <adam.dilger@geometry.com.au>
2022-09-21 13:34:41 +1000

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,43 @@
+type Node<T> = {
+	value: T,
+	prev?: Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
-
+		private head?: Node<T>;
     
 
     constructor() {
+			this.head = undefined;
+			this.length = 0;
     }
 
-    push(item: T): void {
+		push(item: T): void {
+			const node: Node<T> = { value: item }
 
-}
-    pop(): T | undefined {
+			this.length++;
 
-}
-    peek(): T | undefined {
+			if (!this.head) {
+				this.head = node;
+				return;
+			}
 
-}
+			node.prev = this.head;
+			this.head = node;
+		}
+
+		pop(): T | undefined {
+			if (!this.head) return undefined;
+
+			this.length--;
+			const head = this.head;
+			this.head = this.head.prev;
+
+			return head?.value;
+		}
+
+		peek(): T | undefined {
+			return this.head?.value;
+		}
 }
