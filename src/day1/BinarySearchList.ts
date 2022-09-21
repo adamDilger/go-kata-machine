a8aa574db3a9cc6f948ea764f007698083ae6356
Adam Dilger <adam.dilger@geometry.com.au>
2022-09-21 14:51:58 +1000

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+	let lo = 0;
+	let hi = haystack.length;
+	let m = Math.floor(((hi - lo) / 2) + lo);
 
+	do {
+		if (haystack[m] === needle) {
+			return true;
+		}
+
+		if (needle < haystack[m]) {
+			hi = m - 1;
+		}
+		else {
+			lo = m + 1;
+		}
+
+		m = Math.floor(((hi - lo) / 2) + lo);
+	} while (lo < hi)
+
+	return false;
 }
