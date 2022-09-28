d7c44a2b0a2c40c04c0c81290639ef56dc9b5029
Adam Dilger <adam.dilger@geometry.com.au>
2022-09-28 14:55:04 +1000

    compare binary tree

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,11 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+function compare_bst(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+	if (a == null && b == null) return true;
+	if (a == null || b == null) return false;
+	if (a.value !== b.value) return false;
 
+	return compare_bst(a.left, b.left) && compare_bst(a.right, b.right);
+}
+
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+	return compare_bst(a, b);
 }
